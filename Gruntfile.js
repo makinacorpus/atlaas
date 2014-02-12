18cfa09d426207f64fa7e7b74a40fa6bd8e049c3
Adrien Grsmto <grsmto@gmail.com>
2014-02-12 15:53:13 +0100

    Fix a bug were Grunt watcher had high cpu usage

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
                 files: [
                     '<%= yeoman.app %>/*.html',
                     '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
-                    '{.tmp,<%= yeoman.app %>}/**/*.js',
+                    '{.tmp,<%= yeoman.app %>}/scripts/{,*/}/{,*/}*.js',
                     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
                     '<%= yeoman.app %>/scripts/templates/*.{ejs,mustache,hbs}',
                     'test/spec/**/*.js'
