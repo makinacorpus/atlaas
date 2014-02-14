900a7e6e2d8047ad2e4b7dbc731508d8d0e6dbee
Adrien Grsmto <grsmto@gmail.com>
2014-02-14 10:34:30 +0100

    Better map design by Fred

--- app/scripts/views/map/map-view.js
+++ app/scripts/views/map/map-view.js
@@ -29,7 +29,7 @@ atlaas.Views.Map = atlaas.Views.Map || {};
             this.map = L.map(this.options.map).setView([46.883, 2.872], 6);
 
             // add an OpenStreetMap tile layer
-            L.tileLayer('http://{s}.tile.cloudmade.com/6f15a6651c8849349feea8b81a207bc1/997/256/{z}/{x}/{y}.png', {
+            L.tileLayer('http://{s}.livembtiles.makina-corpus.net/makina/osmlight-france/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(this.map);
 
