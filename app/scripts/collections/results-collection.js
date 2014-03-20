cbc081bd09319418a425de740b33b664653d113b
Adrien Grsmto <grsmto@gmail.com>
2014-03-20 15:51:20 +0100

    Add separate collection for search results

---
+++ app/scripts/collections/results-collection.js
@@ -0,0 +1,12 @@
+/*global atlaas, Backbone*/
+
+atlaas.Collections = atlaas.Collections || {};
+
+(function () {
+    'use strict';
+
+    atlaas.Collections.ResultsCollection = atlaas.Collections.PoisCollection.extend({
+
+    });
+
+})();
